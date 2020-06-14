79d5407c3457a06178b113d4144717dd07ca4078
ssight <ssight@protonmail.com>
2020-06-14 11:18:25 +0100

    Added the index.ts file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import checkPlatform from './lib/platform-check';
+import { win32, linux } from './lib/platforms';
+
+const platform = checkPlatform();
+export async function prompt(title: string, body: string, defaultText: string = ""): Promise<string | null> {
+    switch (platform) {
+        case 'win32': return await win32(title, body, defaultText);
+        case 'linux': return await linux(title, body, defaultText);
+    }
+}
