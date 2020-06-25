d60de1a321c38e70ff885758c8d6d010cb5be1a2
ssight <ssight@protonmail.com>
2020-06-25 09:51:20 +0100

    Made prompt the default export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import checkPlatform from './lib/platform-check';
 import { win32, linux, darwin } from './lib/platforms';
 
 const platform = checkPlatform();
-export async function prompt(title: string, body: string, defaultText: string = ""): Promise<string | null> {
+export = async function (title: string, body: string, defaultText: string = ""): Promise<string | null> {
     switch (platform) {
         case 'win32': return await win32(title, body, defaultText);
         case 'linux': return await linux(title, body, defaultText);
