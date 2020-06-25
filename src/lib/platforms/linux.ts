28f1092d6c4ec992161a57f8510aad55462d6ef4
ssight <ssight@protonmail.com>
2020-06-25 16:15:17 +0100

    Added the mask option for Linux

--- src/lib/platforms/linux.ts
+++ src/lib/platforms/linux.ts
@@ -1,9 +1,9 @@
 import { spawn } from 'child_process';
 import { resolve as path } from 'path';
 
-export = function (title: string, body: string, defaultText: string = ""): Promise<string | null> {
+export = function (title: string, body: string, mask: boolean, defaultText: string = ""): Promise<string | null> {
     return new Promise(resolve => {
-        const boxSpawner = spawn("bash", [path(__dirname, "../../../", "./native/linux.sh"), title, body, defaultText]);
+        const boxSpawner = spawn("bash", [path(__dirname, "../../../", "./native/linux/default.sh"), title, body, defaultText, mask === true ? "--hide-text" : ""]);
 
         boxSpawner.stdout.on('data', d => {
             const data = d.toString() as string;
