2007457c2d5b101ba6d1fc101ba246ae3e0176df
ssight <ssight@protonmail.com>
2020-06-25 16:14:28 +0100

    Added the mask option for MacOS

--- src/lib/platforms/darwin.ts
+++ src/lib/platforms/darwin.ts
@@ -1,9 +1,10 @@
 import { spawn } from 'child_process';
 import { resolve as path } from 'path';
 
-export = function (title: string, body: string, defaultText: string = ""): Promise<string | null> {
+export = function (title: string, body: string, mask: boolean, defaultText: string = ""): Promise<string | null> {
     return new Promise(resolve => {
-        const boxSpawner = spawn("osascript", [path(__dirname, "../../../", "./native/darwin.scpt"), title, body, defaultText]);
+        const nativePath = mask ? "./native/darwin/mask.scpt" : "./native/darwin/default.scpt";
+        const boxSpawner = spawn("osascript", [path(__dirname, "../../../", nativePath), title, body, defaultText]);
 
         boxSpawner.stdout.on('data', d => {
             const data = d.toString() as string;
